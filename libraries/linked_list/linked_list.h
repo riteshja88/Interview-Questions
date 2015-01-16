62c2a9c428b2a7c73dd79889999db1625619e08e
Ritesh Agarwal(Omi) <riteshja88@gmail.com>
2015-01-16 17:49:47 -0600

    Renamings done

---
+++ libraries/linked_list/linked_list.h
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  linked_list.h
+ *
+ *    Description:  Linked List library
+ *
+ *        Version:  1.0
+ *        Created:  01/16/2015 05:43:07 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ritesh Agarwal (Omi), riteshja@yahoo-inc.com
+ *        Company:  Yahoo! Inc
+ *
+ * =====================================================================================
+ */
+
+typedef struct node_t node_t;
+
+
