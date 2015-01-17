c09ffed337b792d92f42ad406bc4b64be670da1f
Ritesh Agarwal(Omi) <riteshja88@gmail.com>
2015-01-16 20:05:52 -0600

    Linked list function signatures

--- libraries/linked_list/linked_list.h
+++ libraries/linked_list/linked_list.h
@@ -16,6 +16,8 @@
  * =====================================================================================
  */
 
+#include <inttypes.h>
+
 typedef struct node_t node_t;
 
 void llist_init(node_t **);
@@ -28,7 +30,7 @@ void llist_add_tail(node_t **, void *);
 
 void llist_add_priority(node_t **, void *, int (*cmp_func)(void *, void *));
 
-uin_8 llist_count(node_t *);
+uint64_t llist_count(node_t *);
 
 
 
