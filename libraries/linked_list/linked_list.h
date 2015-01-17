d6556cc63726c9d3137655a532c32f20f136a685
Ritesh Agarwal(Omi) <riteshja88@gmail.com>
2015-01-16 18:10:07 -0600

    Linked List method signatures .... part 1

--- libraries/linked_list/linked_list.h
+++ libraries/linked_list/linked_list.h
@@ -18,4 +18,17 @@
 
 typedef struct node_t node_t;
 
+void llist_init(node_t **);
+
+void llist_add(node_t **, void *);
+
+void llist_add_head(node_t **, void *);
+
+void llist_add_tail(node_t **, void *);
+
+void llist_add_priority(node_t **, void *, int (*cmp_func)(void *, void *));
+
+uin_8 llist_count(node_t *);
+
+
 
